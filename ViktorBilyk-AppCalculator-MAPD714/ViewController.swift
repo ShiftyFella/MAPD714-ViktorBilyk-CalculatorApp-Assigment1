b9c141c7ee0c8bb9b6a30bd56849bd832e18fa01
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-26 12:31:53 -0400

    Basic Calc Logic Added function to do basic calculations

--- ViktorBilyk-AppCalculator-MAPD714/ViewController.swift
+++ ViktorBilyk-AppCalculator-MAPD714/ViewController.swift
@@ -1,6 +1,6 @@
 //
-//  ViewController.swift
-//  ViktorBilyk-AppCalculator-MAPD714
+//  Assigment 1 Calculator App
+//  Version: 0.3 Basic Calculator Logic
 //
 //  Created by Viktor Bilyk on 2017-09-26.
 //  Copyright © 2017 Shifty Land LLC. All rights reserved.
@@ -19,7 +19,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Calculator variables
+    var firstOperand:Double = 0 // left side of calculations
+    var secondOperand:Double = 0 // right side of calcultions
+    var calcResult:Double = 0 // result of calculations
+    var isMath:Bool = false // is Math operation pressed
+    var currentMathOperation:Int = 0 // selected ID of operation
+    
+    //function to do basic calculations
+    func calculations () -> Double {
+        var tempResult:Double = 0
+        switch currentMathOperation {
+        case 11:
+            tempResult = firstOperand + secondOperand
+        case 12:
+            tempResult = firstOperand - secondOperand
+        case 13:
+            tempResult = firstOperand * secondOperand
+        case 14:
+            tempResult = firstOperand / secondOperand
+        default: break
+        }
+        return tempResult
+    }
 
 }
 
