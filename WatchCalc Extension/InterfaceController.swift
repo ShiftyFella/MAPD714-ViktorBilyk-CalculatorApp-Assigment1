6c3137ec3cf5ebffeee7d64e6d853a1ea45dc0ec
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-27 15:39:13 -0400

    WatchKit test APP

---
+++ WatchCalc Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchCalc Extension
+//
+//  Created by Viktor Bilyk on 2017-09-27.
+//  Copyright © 2017 Shifty Land LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
